f4a3e55f6faf68bd28846f154c3a476af00f2ee6
Vrinnda <60807316+Vrinnda@users.noreply.github.com>
2020-11-06 21:25:18 +0530

    Update easyui.R

--- easyui.R
+++ easyui.R
@@ -17,19 +17,19 @@ ui <- fluidPage(
         sidebarPanel(
             
             # Using easier options and other things
-            textInput("title", "Title", "GDP vs life exp"),
-            numericInput("size", "Point size", 1, 1),
-            checkboxInput("fit", "Add line of best fit", FALSE),
-            colourInput("color", "Point color", value = "blue"),
+            textInput("main", "main", "avoid plagiarism"),
+            numericInput("main", "main", 1, 1),
+            checkboxInput("fi-t", "best fi-t", FALSE),
+            colourInput("shades", "shadingg", value = "green"),
             
             
-            selectInput("continent", "Continent",
-                        choices = c("All", levels(gapminder$continent))),
+            selectInput("county", "County",
+                        choices = c("everything", levels(gapminder$continent))),
             
-            sliderInput(inputId = "life", label = "Life expectancy",
+            sliderInput(inputId = "easy", label = "peasy",
                         min = 0, max = 120,
                         value = c(30, 50)),
-            downloadButton("download_data")
+            downloadButton("itsdonenow")
             
             
         ),
@@ -38,10 +38,10 @@ ui <- fluidPage(
         mainPanel(
             
             # The final outcome using various features and stuff
-            tabsetPanel(type = "tabs",
+            tabsetPanel(type = "finals",
                         
-                        tabPanel("Plot", withLoader(plotlyOutput("plot")) ),
-                        tabPanel("Table", withLoader(DT::dataTableOutput("table")))
+                        tabPanel("Plottingmax", withLoader(plotlyOutput("plottingmax")) ),
+                        tabPanel("Tabulature", withLoader(DT::dataTableOutput("tabulature")))
                  
                         
             )
